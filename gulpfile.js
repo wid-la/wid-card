6f4693ee751234cdd1586e3e79184812ba6b9d64
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-07-10 17:29:43 -0700

    Lifting of bower and package.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp'),
 	bs = require('browser-sync').create(),
 	bg = require("gulp-bg"),
 	ps = require('ps-node');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var tag_version = require('gulp-tag-version');
 
 var POLYSERVE_PORT = 8080,
     elementName = 'wid-card';
@@ -55,3 +58,20 @@ gulp.task('test:watch', ['polyserve'], function(cb) {
 });
 
 try { require('web-component-tester').gulp.init(gulp); } catch (err) {}
+
+function inc(importance) {
+  // get all the files to bump version in
+  return gulp.src(['./bower.json'])
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumps package version'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
