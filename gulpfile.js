8284415a35b394221d7bfae8576995fc2cdabb91
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-07-10 17:32:07 -0700

    update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
 var git = require('gulp-git');
 var bump = require('gulp-bump');
 var tag_version = require('gulp-tag-version');
+var filter = require('gulp-filter');
 
 var POLYSERVE_PORT = 8080,
     elementName = 'wid-card';
@@ -61,13 +62,15 @@ try { require('web-component-tester').gulp.init(gulp); } catch (err) {}
 
 function inc(importance) {
   // get all the files to bump version in
-  return gulp.src(['./bower.json'])
+  return gulp.src(['./package.json', './bower.json'])
     // bump the version number in those files
     .pipe(bump({type: importance}))
     // save it back to filesystem
     .pipe(gulp.dest('./'))
     // commit the changed version number
     .pipe(git.commit('bumps package version'))
+    // read only one file to get the version number 
+    .pipe(filter('bower.json'))
     // **tag it in the repository**
     .pipe(tag_version());
 }
